b6b75bedaea2f67fac796d73e7ab04c3ab3da295
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-01-30 00:25:15 +0700

    update data-theme

--- docs/_next/static/chunks/5be92831bd4b6c97.js
+++ docs/_next/static/chunks/5be92831bd4b6c97.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/7f323f0b5c230923.js",
   "static/chunks/7be47748cbe58f57.js",
   "static/chunks/6fc7c4c60e42193d.js",
-  "static/chunks/3d61ea95fd06c8ee.css",
-  "static/chunks/turbopack-9049c0025d6559bb.js"
+  "static/chunks/7ef0144be1cf3188.css",
+  "static/chunks/turbopack-2c42003fcc6b78f6.js"
 ])
